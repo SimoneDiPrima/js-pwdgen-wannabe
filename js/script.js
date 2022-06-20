4325055fa97ea8d04058f032e6e509377fc41001
SimoneDiPrima <simonediprimagenova@gmail.com>
2022-06-20 14:48:58 +0200

    first commit

---
+++ js/script.js
@@ -0,0 +1,13 @@
+//analisi problema diviso per sezioni
+/*1-chiedi all utente il suo nome;
+2-inserisci il suo nome in una variabile;
+3.chiedi all utente il suo cognome;
+inserisci il suo cognome in una variabile;
+4.chiedi il suo colore preferito e inseriscilo in una variabile;
+5-stampa la nuova password generata con nome+cognome+colore+21*/
+//prima di tutto devo inizializzare le variabili:
+
+const Name = prompt(`Come ti chiami?` , `scrivi il tuo nome`);
+console.log(Name);
+const Surname = prompt(`Qual e il tuo cognome??` , `scrivi il tuo cognome`);
+console.log(Surname);
